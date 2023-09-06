a7b46f1ff3d33addd33eb55b65c8750e6ae29ba0
hoang <nnhoanghd2004@gmail.com>
2023-09-06 18:02:20 +0700

    #9.4.3 Fix CORS

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom"
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function Register() {
     const navigate = useNavigate();
+    useEffect(() => {
+        axios.get('http://localhost:8080/test/api')
+            .then(data => console.log(data))
+    }, [])
     return (
         <div className='container pt-5' >
             <div className='row'>
@@ -12,54 +18,54 @@ export default function Register() {
                         <h2 className='d-flex '>Hoang</h2>
                     </div>
                     <form>
-                        <div class="form-outline my-3">
-                            <input type="email" id="form2Example1" class="form-control py-3" placeholder='Email address' />
+                        <div className="form-outline my-3">
+                            <input type="email" id="form2Example1" className="form-control py-3" placeholder='Email address' />
                         </div>
 
-                        <div class="form-outline my-3">
-                            <input type="password" id="form2Example2" class="form-control py-3" placeholder='Password' />
+                        <div className="form-outline my-3">
+                            <input type="password" id="form2Example2" className="form-control py-3" placeholder='Password' />
                         </div>
 
-                        <div class="form-outline my-3">
-                            <input type="text" id="form2Example1" class="form-control py-3" placeholder='Username' />
+                        <div className="form-outline my-3">
+                            <input type="text" id="form2Example1" className="form-control py-3" placeholder='Username' />
                         </div>
 
-                        <div class="form-outline my-3">
-                            <input type="text" id="form2Example2" class="form-control py-3" placeholder='Address' />
+                        <div className="form-outline my-3">
+                            <input type="text" id="form2Example2" className="form-control py-3" placeholder='Address' />
                         </div>
 
-                        <div class="form-outline my-3">
-                            <input type="text" id="form2Example1" class="form-control py-3" placeholder='Phone' />
+                        <div className="form-outline my-3">
+                            <input type="text" id="form2Example1" className="form-control py-3" placeholder='Phone' />
                         </div>
 
                         <div>Sex</div>
-                        <div class="input-group">
-                            <div class="input-group-text">
-                                <input class="mt-0" type="radio" name='sex' />
+                        <div className="input-group">
+                            <div className="input-group-text">
+                                <input className="mt-0" type="radio" name='sex' />
                             </div>
-                            <input type="text" class="form-control" value="Male" />
-                            <div class="input-group-text">
-                                <input class="mt-0" type="radio" name='sex' />
+                            <input type="text" className="form-control" defaultValue="Male" />
+                            <div className="input-group-text">
+                                <input className="mt-0" type="radio" name='sex' />
                             </div>
-                            <input type="text" class="form-control" value="Male" />
+                            <input type="text" className="form-control" defaultValue="Male" />
                         </div>
-                        {/* <div class="form-outline my-3">
-                            <input type="radio" id="Male" class="form-control py-3" />
+                        {/* <div className="form-outline my-3">
+                            <input type="radio" id="Male" className="form-control py-3" />
                             <label for="html">Male</label><br />
-                            <input type="radio" id="Femal" class="form-control py-3" />
+                            <input type="radio" id="Femal" className="form-control py-3" />
                             <label for="html">Femal</label><br />
-                            <input type="radio" id="Other" class="form-control py-3" />
+                            <input type="radio" id="Other" className="form-control py-3" />
                             <label for="html">Other</label><br />
                         </div> */}
 
-                        <div class="row my-4">
-                            <div class="col">
+                        <div className="row my-4">
+                            <div className="col">
                                 <a href="#!">Forgot password?</a>
                             </div>
                         </div>
                         <div className='row mt-3 d-flex justify-content-around'>
-                            <button type="button" class="btn btn-success btn-block mb-4 col-5">Register</button>
-                            <button class="btn btn-secondary btn-block mb-4 col-5" onClick={() => navigate("/login")}>
+                            <button type="button" className="btn btn-success btn-block mb-4 col-5">Register</button>
+                            <button className="btn btn-secondary btn-block mb-4 col-5" onClick={() => navigate("/login")}>
                                 Login
                             </button>
                         </div>
