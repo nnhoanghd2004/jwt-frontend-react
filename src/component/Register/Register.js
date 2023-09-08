bb77557d177806a7ef342d3bd6de1532eb0e2bb4
hoang <nnhoanghd2004@gmail.com>
2023-09-08 14:21:18 +0700

    #10.1.3 Axios Post Data - Test APIs With React

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -6,7 +6,18 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Register() {
+
+    const lowerCaseLetters = /[a-z]/g;
+    const upperCaseLetters = /[A-Z]/g;
+    const numbers = /[0-9]/g;
+    const phoneValidate = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    let defaultValidInput = {
+        emailValid: true, passwordValid: true, rePasswordValid: true,
+        usernameValid: true, addressValid: true, phoneValid: true
+    }
+
     const navigate = useNavigate();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [rePassword, setRePassword] = useState("");
@@ -14,65 +25,107 @@ export default function Register() {
     const [address, setAddress] = useState("");
     const [phone, setPhone] = useState("");
     const [sex, setSex] = useState("Male");
-    const validateData = () => {
-        const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-        if (!reg.test(email)) {
-            toast.warn("Invalid email");
-            return false;
+    const [valid, setValid] = useState(defaultValidInput)
+
+    const handleSubmit = () => {
+        if (defaultValidInput.emailValid && defaultValidInput.passwordValid && defaultValidInput.rePasswordValid &&
+            defaultValidInput.usernameValid && defaultValidInput.addressValid && defaultValidInput.phoneValid) {
+            if (password, email, rePassword, username, address, phone) {
+                axios.post('http://localhost:8080/api/v1/register', {
+                    email, password, username, address, phone, sex
+                })
+                navigate('/login')
+            }
         }
-
-        const lowerCaseLetters = /[a-z]/g;
-        const upperCaseLetters = /[A-Z]/g;
-        const numbers = /[0-9]/g;
-        const phoneValidate = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-
+    }
+    const validateEmail = () => {
+        const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (!email) {
+            toast.error("Your email is empty");
+            setValid({ ...valid, emailValid: false });
+            return;
+        }
+        if (!reg.test(email)) {
+            toast.error("Invalid email");
+            valid.emailValid = false;
+            setValid({ ...valid, emailValid: false });
+            return;
+        }
+        setValid({ ...valid, emailValid: true });
+    }
+    const validatePassword = () => {
+        if (!password) {
+            toast.error("Your password is empty");
+            setValid({ ...valid, passwordValid: false });
+            return;
+        }
         if (password.length > 8) {
             if (!password.match(lowerCaseLetters)) {
-                toast.warn("Your password requires lowercase letters");
-                return false;
+                toast.error("Your password requires lowercase letters");
+                setValid({ ...valid, passwordValid: false });
             } else if (!password.match(upperCaseLetters)) {
-                toast.warn("Your password requires uppercase letters");
-                return false;
+                toast.error("Your password requires uppercase letters");
+                setValid({ ...valid, passwordValid: false });
             } else if (!password.match(numbers)) {
-                toast.warn("Your password requires numbers");
-                return false;
+                toast.error("Your password requires numbers");
+                setValid({ ...valid, passwordValid: false });
             }
         } else {
-            toast.warn("Your password requires numbers");
-            return false;
+            toast.error("Your password less than 8 characters");
+            setValid({ ...valid, passwordValid: false });
         }
-
+        setValid({ ...valid, passwordValid: true });
+    }
+    const validateRePassword = () => {
+        if (!rePassword) {
+            toast.error("Your Re-Password is empty");
+            setValid({ ...valid, rePasswordValid: false });
+            return;
+        }
         if (rePassword !== password) {
-            toast.warn("Your passwords are not the same");
-            return false;
+            toast.error("Your passwords are not the same");
+            setValid({ ...valid, rePasswordValid: false });
+            return;
         }
-
+        setValid({ ...valid, rePasswordValid: true });
+    }
+    const validateUsername = () => {
         if (!username) {
-            toast.warn("Your username is empty");
-            return false;
+            toast.error("Your username is empty");
+            setValid({ ...valid, usernameValid: false });
+            return;
         }
-
+        setValid({ ...valid, usernameValid: true });
+    }
+    const validateAddress = () => {
         if (!address) {
-            toast.warn("Your address is empty");
-            return false;
+            toast.error("Your address is empty");
+            setValid({ ...valid, addressValid: false });
+            return;
         }
-
-        if (!phoneValidate.test(phone)) {
-            toast.warn("Invalide phone number");
-            return false;
+        setValid({ ...valid, addressValid: true });
+    }
+    const validatePhone = () => {
+        if (!phone) {
+            toast.error("Your phone is empty");
+            setValid({ ...valid, phoneValid: false });
+            return;
         }
-
-        return true;
-    }
-    const handleRegister = () => {
-        if (validateData())
-            console.log({ email, password, username, address, phone, sex });
+        setValid({ ...valid, phoneValid: true });
     }
 
-    // useEffect(() => {
-    //     axios.get('http://localhost:8080/test/api')
-    //         .then(data => console.log(data))
-    // }, [])
+    const handleClick = (id) => {
+        const key = id + "Valid"
+        setValid({ ...valid, [key]: true });
+    }
+
+    useEffect(() => {
+        // axios.get('http://localhost:8080/api/v1/test-api')
+        //     .then(data => console.log(data))
+        axios.post('http://localhost:8080/api/v1/register', {
+            email, password, username, address, phone, sex
+        })
+    }, [])
 
     return (
         <div className='container pt-5' >
@@ -80,37 +133,43 @@ export default function Register() {
                 <div className='col-md-2 col-lg-3 col-xl-4'></div>
                 <div className='col-sm-12 col-md-8 col-lg-6 col-xl-4  rounded' style={{ backgroundColor: "white" }}>
                     <div className='d-flex justify-content-center mt-3'>
-                        <h2 className='d-flex '>Hoang</h2>
+                        <h2 className='d-flex'>Hoang</h2>
                     </div>
                     <form>
                         <div className="form-outline my-3">
-                            <input type="email" id="form2Example1" className="form-control py-3"
-                                placeholder='Email address' value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input type="email" id="email" placeholder='Email address' value={email}
+                                className={valid.emailValid ? "form-control py-3" : "form-control py-3 is-invalid"}
+                                onChange={(e) => setEmail(e.target.value)} onBlur={() => validateEmail()} onClick={(e) => handleClick(e.target.id)} />
                         </div>
 
                         <div className="form-outline my-3">
-                            <input type="password" id="form2Example2" className="form-control py-3"
-                                placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <input type="password" id="password" placeholder='Password' value={password}
+                                className={valid.passwordValid ? "form-control py-3" : "form-control py-3 is-invalid"}
+                                onChange={(e) => setPassword(e.target.value)} onBlur={() => validatePassword()} onClick={(e) => handleClick(e.target.id)} />
                         </div>
 
                         <div className="form-outline my-3">
-                            <input type="password" id="form2Example2" className="form-control py-3"
-                                placeholder='Re-Password' value={rePassword} onChange={(e) => setRePassword(e.target.value)} />
+                            <input type="password" id="rePassword" placeholder='Re-Password' value={rePassword}
+                                className={valid.rePasswordValid ? "form-control py-3" : "form-control py-3 is-invalid"}
+                                onChange={(e) => setRePassword(e.target.value)} onBlur={() => validateRePassword()} onClick={(e) => handleClick(e.target.id)} />
                         </div>
 
                         <div className="form-outline my-3">
-                            <input type="text" id="form2Example1" className="form-control py-3"
-                                placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <input type="text" id="username" placeholder='Username' value={username}
+                                className={valid.usernameValid ? "form-control py-3" : "form-control py-3 is-invalid"}
+                                onChange={(e) => setUsername(e.target.value)} onBlur={() => validateUsername()} onClick={(e) => handleClick(e.target.id)} />
                         </div>
 
                         <div className="form-outline my-3">
-                            <input type="text" id="form2Example2" className="form-control py-3"
-                                placeholder='Address' value={address} onChange={(e) => setAddress(e.target.value)} />
+                            <input type="text" id="address" placeholder='Address' value={address}
+                                className={valid.addressValid ? "form-control py-3" : "form-control py-3 is-invalid"}
+                                onChange={(e) => setAddress(e.target.value)} onBlur={() => validateAddress()} onClick={(e) => handleClick(e.target.id)} />
                         </div>
 
                         <div className="form-outline my-3">
-                            <input type="text" id="form2Example1" className="form-control py-3"
-                                placeholder='Phone' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                            <input type="text" id="phone" placeholder='Phone' value={phone}
+                                className={valid.phoneValid ? "form-control py-3" : "form-control py-3 is-invalid"}
+                                onChange={(e) => setPhone(e.target.value)} onBlur={() => validatePhone()} onClick={(e) => handleClick(e.target.id)} />
                         </div>
 
                         <div>Sex</div>
@@ -139,7 +198,7 @@ export default function Register() {
                             </div>
                         </div>
                         <div className='row mt-3 d-flex justify-content-around'>
-                            <button type="button" className="btn btn-success btn-block mb-4 col-5" onClick={() => handleRegister()}>Register</button>
+                            <button type="button" className="btn btn-success btn-block mb-4 col-5" onClick={() => handleSubmit()}>Register</button>
                             <button className="btn btn-secondary btn-block mb-4 col-5" onClick={() => navigate("/login")}>
                                 Login
                             </button>
